53020261efcc6437c464fa09d121c74127d4615f
Haroldo de Oliveira Pinheiro <haroldoop@gmail.com>
2015-12-01 11:15:48 -0200

    Skeleton of the application

---
+++ road.js
@@ -0,0 +1,10 @@
+'use strict';
+
+(function(){
+  var roadImage = new Image();
+  roadImage.onload = function(){
+    var canvas = document.getElementById('game-canvas');
+    alert('!');
+  };
+  roadImage.src = 'road04_13.png';
+})();

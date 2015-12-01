dfe1fb436f88da9ab1878f7e4fc46fe96f497112
Haroldo de Oliveira Pinheiro <haroldoop@gmail.com>
2015-12-01 11:44:27 -0200

    Zoom implemented

--- road.js
+++ road.js
@@ -6,6 +6,9 @@
     var canvas = document.getElementById('game-canvas'),
         scrW = canvas.width,
         scrH = canvas.height,
+        scrDepth = scrW,
+        centerX = scrW / 2,
+        centerY = scrH / 2,
         imgW = roadImage.width,
         imgH = roadImage.height,
         imgPartW = imgW / 2,
@@ -19,7 +22,23 @@
 
     ctx.fillRect(0, 0, scrW, scrH);
 
-    ctx.drawImage(roadImage, 0, 0, imgPartW, imgPartH, 0, 0, imgPartW, imgPartH);
+    function drawRoadSlice(scale, number) {
+      var destW = imgPartW * scale,
+          destH = imgPartH * scale,
+          destX = centerX - destW / 2,
+          destY = centerY - destH / 2;
+
+      ctx.drawImage(roadImage, 0, imgPartH * number, imgPartW, imgPartH,
+                    destX, destY, destW, destH);
+    }
+
+    drawRoadSlice(0.5, 0);
+    /*
+    for (var z = scrDepth; z >= 1; z -= 4) {
+      var scale = 1.0 * scrDepth / (z * scrDepth);
+      drawRoadSlice(scale, 0);
+    }
+    */
   };
   roadImage.src = 'road04_13.png';
 })();
